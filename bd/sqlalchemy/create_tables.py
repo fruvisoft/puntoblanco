ad2843938e8445c1b2c0e7ec5e8f2390b0d62836
Kevin Zuniga <kevin.zun@gmail.com>
2013-12-06 08:24:07 -0500

    redone

--- bd/sqlalchemy/create_tables.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from engine import engine
-
-metadata.create_all(engine)
-   
--- wsgi.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from puntoblanco import app
-
-application = app
-
-if __name__ == '__main__':
-    app.run(debug=True)
