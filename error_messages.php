03b79261cf430183413080b1241a2c062a4f76c2
Kevin Zuniga <kevin.zun@gmail.com>
2013-12-06 09:31:26 -0500

    added some error messages

--- error_messages.php
+++ error_messages.php
@@ -2,3 +2,8 @@
     $error_msg[1001] = 'No existe el usuario';
     $error_msg[1002] = 'La contraseña es incorrecta';
     $error_msg[1003] = 'No tiene permisos para ver esta página';
+    
+    $error_msg[1100] = 'No hay mesa';
+    $error_msg[1101] = 'No hay mesa activa';
+    $error_msg[1102] = 'La mesa seleccionada no es la mesa activa';
+    $error_msg[1103] = 'No hay orden';
